47710671eb51e8cdf9b6776d8ad5740048b5ef87
githubmutt <97822530+githubmutt@users.noreply.github.com>
2022-12-02 00:10:12 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const employeeDatabase = require("./employeeDatabase.js");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+
+app.listen(3000, () => console.log('port #3000: server started'));
